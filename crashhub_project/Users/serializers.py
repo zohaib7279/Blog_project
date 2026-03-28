8bdd5abde85ccd5a6eee76ae2dbb080db76ddda3
Zohaib Abdul Jabbar <zohaib7279@gmail.com>
2026-03-28 09:33:00 +0500

    edit value change

--- crashhub_project/Users/serializers.py
+++ crashhub_project/Users/serializers.py
@@ -1,7 +1,10 @@
+from os import read
+
 from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields =  ['id', 'username', 'email', 'password']
+        read_only_fields = ['id']
